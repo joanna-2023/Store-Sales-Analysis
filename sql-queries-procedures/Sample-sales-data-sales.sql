ff22ebf11f3ecee9c9ae77282944eaa69ee282a4
joanna-2023 <140013312+joanna-2023@users.noreply.github.com>
2023-08-03 11:41:23 -0600

    Sample data.

---
+++ sql-queries-procedures/Sample-sales-data-sales.sql
@@ -0,0 +1,32 @@
+# Inserting sample data - 
+
+#TRUNCATE retailsales.sales;
+#Sales:
+
+#Sales in Calgary
+INSERT INTO retailsales.sales (productId,salesPersonId,storeId,date,unitsCount,onlinePurchase,customerId) 
+VALUES (12344,100,110,"12/01/2022",3,FALSE,200);
+INSERT INTO retailsales.sales (productId,salesPersonId,storeId,date,unitsCount,onlinePurchase,customerId) 
+VALUES (12345,100,110,"12/01/2022",3,FALSE,200);
+INSERT INTO retailsales.sales (productId,salesPersonId,storeId,date,unitsCount,onlinePurchase,customerId) 
+VALUES (12345,100,110,"12/01/2022",5,FALSE,200);
+
+INSERT INTO retailsales.sales (productId,salesPersonId,storeId,date,unitsCount,onlinePurchase,customerId) 
+VALUES (12344,100,110,"12/01/2022",1,FALSE,201);
+INSERT INTO retailsales.sales (productId,salesPersonId,storeId,date,unitsCount,onlinePurchase,customerId) 
+VALUES (12345,100,110,"12/01/2022",2,FALSE,201);
+INSERT INTO retailsales.sales (productId,salesPersonId,storeId,date,unitsCount,onlinePurchase,customerId) 
+VALUES (12345,100,110,"12/01/2022",3,FALSE,201);
+INSERT INTO retailsales.sales (productId,salesPersonId,storeId,date,unitsCount,onlinePurchase,customerId) 
+VALUES (12346,100,110,"12/01/2022",3,FALSE,201);
+
+INSERT INTO retailsales.sales (productId,salesPersonId,storeId,date,unitsCount,onlinePurchase,customerId) 
+VALUES (12344,100,110,"12/01/2022",1,FALSE,202);
+INSERT INTO retailsales.sales (productId,salesPersonId,storeId,date,unitsCount,onlinePurchase,customerId) 
+VALUES (12345,100,110,"12/01/2022",2,FALSE,202);
+INSERT INTO retailsales.sales (productId,salesPersonId,storeId,date,unitsCount,onlinePurchase,customerId) 
+VALUES (12345,100,110,"12/01/2022",1,FALSE,202);
+INSERT INTO retailsales.sales (productId,salesPersonId,storeId,date,unitsCount,onlinePurchase,customerId) 
+VALUES (12346,100,110,"12/01/2022",3,FALSE,202);
+INSERT INTO retailsales.sales (productId,salesPersonId,storeId,date,unitsCount,onlinePurchase,customerId) 
+VALUES (12347,100,110,"12/01/2022",3,FALSE,202);
---
+++ sql-queries-procedures/test-queries.sql
@@ -0,0 +1,4 @@
+SELECT * FROM retailsales.salesPeople;
+#SELECT * FROM retailsales.products;
+#SELECT * FROM retailsales.stores;
+SELECT * FROM retailsales.sales;
